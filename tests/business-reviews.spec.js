46029a2bdd4c24e69ac007bd9725d37fc80c2454
obayedmamur <obayedmamur@gmail.com>
2024-06-23 23:15:04 +0600

    added test case for EA Business Reviews

---
+++ tests/business-reviews.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+import { test, expect } from "../global-setup";
+
+let slug = "/business-reviews";
+let heading = "Business Reviews";
+
+test.describe("Business Reviews", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/ea-business-reviews/);
+  });
+
+  test("Test Section: Style Reviews With Preset 2", async ({ page }) => {
+    await page.getByRole("heading", { name: "Style Reviews With Preset 2" }).scrollIntoViewIfNeeded();
+    await expect(page.getByRole("heading", { name: "Style Reviews With Preset 2" })).toBeVisible();
+  });
+});
