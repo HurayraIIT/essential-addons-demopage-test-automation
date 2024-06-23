400f92cabbc16f4cc77e52049d01bf637fd4517b
Abu Hurayra <hurayraiit@gmail.com>
2024-06-23 14:45:41 +0600

    added boilerplate for some widgets

---
+++ tests/content-toggle.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+import { test, expect } from "../global-setup";
+
+let slug = "/content-toggle";
+let heading = "Content Toggle";
+
+test.describe("Content Toggle", () => {
+  // Setup
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/content-toggle/);
+  });
+
+  test("Test Section: Display Save Template For Content Toggle", async ({ page }) => {
+    await page.getByRole("heading", { name: "Display Save Template For Content Toggle" }).scrollIntoViewIfNeeded();
+    await expect(page.getByRole("heading", { name: "Display Save Template For Content Toggle" })).toBeVisible();
+    await expect(page.getByText("Choose your save template designs & set")).toBeVisible();
+  });
+});
---
+++ tests/offcanvas-content.spec.js
@@ -0,0 +1,24 @@
+"use strict";
+import { test, expect } from "../global-setup";
+
+let slug = "/offcanvas-content";
+let heading = "Offcanvas";
+
+test.describe("Offcanvas", () => {
+  // Setup
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute(
+      "href",
+      /docs\/content-elements\/essential-addons-elementor-offcanvas/
+    );
+  });
+
+  test("Test Section: Configure Offcanvas Settings & Style", async ({ page }) => {
+    await page.getByRole("heading", { name: "Configure Offcanvas Settings & Style" }).scrollIntoViewIfNeeded();
+    await expect(page.getByRole("heading", { name: "Configure Offcanvas Settings & Style" })).toBeVisible();
+    await expect(page.getByText("Choose your preferred ‘Content Type’, add button")).toBeVisible();
+  });
+});
