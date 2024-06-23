c55e3bf87d6734fd087344bfd53152464bfb7e4b
Mohammad Obayed Mamur <obayedmamur@gmail.com>
2024-06-23 16:22:49 +0600

    added test case for EA Post Block

---
+++ tests/post-block.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+import { test, expect } from "../global-setup";
+
+let slug = "/post-block";
+let heading = "Post Block";
+
+test.describe("Post Block", () => {
+  // Setup
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/post-block/);
+  });
+
+  test("Test Section: Tooltip On Bottom Hover Effect", async ({ page }) => {
+    await page.getByRole("heading", { name: "Choose Slide Up Animation" }).scrollIntoViewIfNeeded();
+    await expect(page.getByRole("heading", { name: "Choose Slide Up Animation" })).toBeVisible();
+    await expect(page.getByText("Select the slide up animation to give a stunning look for your WordPress posts")).toBeVisible();
+  });
+});
