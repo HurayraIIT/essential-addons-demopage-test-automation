3afec4648f0aec298e74442d05b36c71770c5e6a
Abu Hurayra <hurayraiit@gmail.com>
2024-06-24 17:29:09 +0600

    added tests for advanced search

--- tests/advanced-search.spec.js
+++ tests/advanced-search.spec.js
@@ -4,18 +4,42 @@ import { test, expect } from "../global-setup";
 let slug = "/advanced-search";
 let heading = "Advanced Search";
 
-test.describe("Advanced Search", () => {
-  // Setup
+test.describe("Advanced Search - Stunning Advanced Live Search Bar", () => {
+  let adv_search = "";
+
   test.beforeEach(async ({ page }) => {
     await page.goto(slug);
     await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
     await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
     await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/advanced-search/);
+
+    adv_search = page.getByTestId("5374321b");
+    adv_search.scrollIntoViewIfNeeded();
   });
 
-  test("Test Section: Stunning Advanced Live Search Bar", async ({ page }) => {
-    await page.getByRole("heading", { name: "Stunning Advanced Live Search Bar" }).scrollIntoViewIfNeeded();
-    await expect(page.getByRole("heading", { name: "Stunning Advanced Live Search Bar" })).toBeVisible();
-    await expect(page.getByText("Add a live advanced search bar that allows your website")).toBeVisible();
+  test("All sections should be present", async ({ page }) => {
+    await expect(adv_search).toBeVisible();
+    await expect(adv_search.locator(".eael-adv-search-wrapper").first()).toBeVisible();
+
+    await expect(adv_search.locator(".fas.fa-search")).toBeVisible();
+    await expect(adv_search.getByPlaceholder("Enter Search Keyword")).toBeVisible();
+    await expect(adv_search.getByRole("button", { name: "Search" })).toBeVisible();
+  });
+
+  test("Search with invalid string should return no result", async ({ page }) => {
+    await expect(adv_search).toBeVisible();
+
+    await adv_search.getByPlaceholder("Enter Search Keyword").click();
+    await adv_search.getByPlaceholder("Enter Search Keyword").type("asdasdasdasdasd123123");
+    await page.waitForTimeout(1500);
+
+    // Verify the result
+    await expect(adv_search.getByText("No Record Found")).toBeVisible();
+    await expect(adv_search.locator(".eael-adv-search-close > .fas.fa-times")).toBeVisible();
+
+    // Clear the search
+    await adv_search.locator(".eael-adv-search-close > .fas.fa-times").click();
+    await page.waitForTimeout(100);
+    await expect(adv_search.getByText("No Record Found")).toBeHidden();
   });
 });
