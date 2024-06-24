f8b3f5932911e4ab4b5e7667005ef6b9b540e9c0
Abu Hurayra <hurayraiit@gmail.com>
2024-06-24 17:16:59 +0600

    added test for advanced menu

--- tests/advanced-menu.spec.js
+++ tests/advanced-menu.spec.js
@@ -4,18 +4,39 @@ import { test, expect } from "../global-setup";
 let slug = "/advanced-menu";
 let heading = "Advanced Menu";
 
-test.describe("Advanced Menu", () => {
-  // Setup
+test.describe("Advanced Menu - Style Default Skin & Horizontal Layout", () => {
+  let menu = "";
+
   test.beforeEach(async ({ page }) => {
     await page.goto(slug);
     await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
     await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
-    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/content-elements\/ea-advanced-menu/);
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute(
+      "href",
+      /docs\/content-elements\/ea-advanced-menu/
+    );
+
+    menu = page.getByTestId("6a863ab4");
+    await menu.scrollIntoViewIfNeeded();
   });
 
-  test("Test Section: Style Default Skin & Horizontal Layout", async ({ page }) => {
-    await page.getByRole("heading", { name: "Style Default Skin & Horizontal Layout" }).scrollIntoViewIfNeeded();
-    await expect(page.getByRole("heading", { name: "Style Default Skin & Horizontal Layout" })).toBeVisible();
-    await expect(page.getByText("Add drop down menu icon, animation effect")).toBeVisible();
+  test("All sections should be present", async ({ page }) => {
+    await expect(menu).toBeVisible();
+
+    await expect(menu.locator("#menu-item-4842").getByRole("link", { name: "Home" })).toBeVisible();
+    await expect(menu.locator("#menu-item-4843").getByRole("link", { name: "Elements" })).toBeVisible();
+    await expect(menu.locator("#menu-item-4859").getByRole("link", { name: "Support" })).toBeVisible();
+    await expect(menu.locator("#menu-item-4861").getByRole("link", { name: "Blog" })).toBeVisible();
+    await expect(menu.locator("#menu-item-4862").getByRole("link", { name: "Contact Us" })).toBeVisible();
+    await expect(menu.locator("#menu-item-4843").getByRole("link", { name: "Elements" })).toBeVisible();
+
+    // Submenu should be hidden
+    await expect(menu.getByRole("link", { name: "Advanced Accordion for" })).toBeHidden();
+    await expect(menu.getByRole("link", { name: "Advanced Menu" })).toBeHidden();
+
+    // Hover the menu item to reveal submenu
+    await menu.locator("#menu-item-4843").getByRole("link", { name: "Elements" }).hover();
+    await expect(menu.getByRole("link", { name: "Advanced Accordion for" })).toBeVisible();
+    await expect(menu.getByRole("link", { name: "Advanced Menu" })).toBeVisible();
   });
 });
