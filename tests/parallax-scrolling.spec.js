5f2de6f0e488061688e0e64bd246b1ebe651d090
Abu Hurayra <hurayraiit@gmail.com>
2024-06-25 17:00:50 +0600

    added tests for parallax scrolling

--- tests/parallax-scrolling.spec.js
+++ tests/parallax-scrolling.spec.js
@@ -4,7 +4,10 @@ import { test, expect } from "../global-setup";
 let slug = "/parallax-scrolling";
 let heading = "Parallax Effect";
 
-test.describe("Parallax Effect", () => {
+test.describe("Parallax Effect - Scroll & Zoom Parallax Effect", () => {
+  let section = "";
+  let parallax_section = "";
+
   test.beforeEach(async ({ page }) => {
     await page.goto(slug);
     await expect(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
@@ -13,11 +16,47 @@ test.describe("Parallax Effect", () => {
       "href",
       /docs\/extensions\/ea-parallax/
     );
+
+    section = page.getByTestId("119946dd");
+    parallax_section = section.locator("#jarallax-container-0");
+
+    await parallax_section.scrollIntoViewIfNeeded();
   });
 
-  test("Test Section: Scroll & Zoom Parallax Effect", async ({ page }) => {
-    await page.getByRole("heading", { name: "Scroll & Zoom Parallax Effect" }).scrollIntoViewIfNeeded();
-    await expect(page.getByRole("heading", { name: "Scroll & Zoom Parallax Effect" })).toBeVisible();
-    await expect(page.getByText("Add beautiful scroll and zoom motion effects")).toBeVisible();
+  test("The sections should load", async ({ page }) => {
+    await expect(parallax_section).toBeVisible();
+
+    await expect(parallax_section).toHaveAttribute(
+      "style",
+      "position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; z-index: -100;"
+    );
+    await expect(parallax_section.locator("div")).toHaveAttribute(
+      "style",
+      /background-position: 50% 50%; background-size: cover; background-repeat: no-repeat; background-image: url\("https:\/\/essential-addons.com\/wp-content\/uploads\/2020\/06\/BG-Image.png"\); position: absolute; top: 0px; left: 0px; width:\s*-?\d+(\.\d+)?px; height:\s*-?\d+(\.\d+)?px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; will-change: transform, opacity; margin-top:\s*-?\d+(\.\d+)?px; transform: translate3d\(0px,\s*-?\d+(\.\d+)?px, 0px\);/
+    );
+  });
+
+  test("Page Scroll should trigger parallax effect", async ({ page }) => {
+    await expect(parallax_section).toBeVisible();
+
+    // Scroll to page top
+    await page.getByRole("heading", { name: heading, exact: true }).scrollIntoViewIfNeeded();
+    await expect(page.getByRole("heading", { name: heading, exact: true })).toBeInViewport();
+
+    // Assert parallax css values
+    await expect(parallax_section.locator("div")).toHaveAttribute(
+      "style",
+      /background-position: 50% 50%; background-size: cover; background-repeat: no-repeat; background-image: url\("https:\/\/essential-addons.com\/wp-content\/uploads\/2020\/06\/BG-Image.png"\); position: absolute; top: 0px; left: 0px; width:\s*\d+(\.\d+)?px; height:\s*\d+(\.\d+)?px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; will-change: transform, opacity; margin-top:\s*-?\d+(\.\d+)?px; transform: translate3d\(0px,\s*-?\d+(\.\d+)?px, 0px\);/
+    );
+
+    // Scroll down
+    await page.getByTestId("d509e6a").scrollIntoViewIfNeeded();
+    await expect(page.getByTestId("d509e6a")).toBeInViewport();
+
+    // Assert parallax css values
+    await expect(parallax_section.locator("div")).toHaveAttribute(
+      "style",
+      /background-position: 50% 50%; background-size: cover; background-repeat: no-repeat; background-image: url\("https:\/\/essential-addons.com\/wp-content\/uploads\/2020\/06\/BG-Image.png"\); position: absolute; top: 0px; left: 0px; width:\s*\d+(\.\d+)?px; height:\s*\d+(\.\d+)?px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; will-change: transform, opacity; margin-top:\s*-?\d+(\.\d+)?px; transform: translate3d\(0px,\s*-?\d+(\.\d+)?px, 0px\);/
+    );
   });
 });
