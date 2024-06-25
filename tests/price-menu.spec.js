8efa1b05d3719ed225d8118907fc14fe0a778c3d
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-06-25 16:34:05 +0600

    Price Menu - Prest 1 Test Case Cover

--- tests/price-menu.spec.js
+++ tests/price-menu.spec.js
@@ -14,8 +14,18 @@ test.describe("Price Menu", () => {
     });
     // Preset 1
     test("Test Section: Showcase Price Menu in Stylish Way", async ({ page }) => {
+
+        const section_root = page.getByTestId('33048ed7')
+
         await page.getByRole("heading", { name: "Showcase Price Menu in Stylish Way" }).scrollIntoViewIfNeeded();
         await expect(page.getByRole("heading", { name: "Showcase Price Menu in Stylish Way" })).toBeVisible();
         await expect(page.getByText("Get ready to design stunning Price Menu within a few mintues")).toBeVisible();
+        await expect(section_root.getByRole('img', { name: 'Price Menu 102' })).toBeVisible();
+        await expect(section_root.getByRole('heading', { name: 'Breakfast Menu' })).toBeVisible();
+        await expect(section_root.getByText('Fig and lemon cake')).toBeVisible();
+        await expect(section_root.getByText('$25')).toBeVisible();
+        const connectors = section_root.locator('.eael-price-title-connector');
+        const isAnyVisible = await connectors.evaluateAll(elements => elements.some(element => element.offsetParent !== null));
+        expect(isAnyVisible).toBe(true);
     });
 });
