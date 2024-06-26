b8ff8090502af542133b496ff9e7cf61c23a4d83
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-06-26 14:40:37 +0600

    Interactive-circle - Visibile Content and click to display

--- tests/interactive-circle.spec.js
+++ tests/interactive-circle.spec.js
@@ -5,17 +5,27 @@ let slug = "/interactive-circle";
 let heading = "Interactive Circle";
 
 test.describe("Interactive Circle", () => {
-    // Setup
-    test.beforeEach(async ({ page }) => {
-        await page.goto(slug);
-        await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
-        await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
-        await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/interactive-circle/);
-    });
+  // Page Heading
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/interactive-circle/);
+  });
 
-    test("Test Section: Choose From Multiple Different Style Layouts", async ({ page }) => {
-        await page.getByRole("heading", { name: "Choose From Multiple Different Style Layouts" }).scrollIntoViewIfNeeded();
-        await expect(page.getByRole("heading", { name: "Choose From Multiple Different Style Layouts" })).toBeVisible();
-        await expect(page.getByText("Beautifully present your content in an attractive ")).toBeVisible();
-    });
+  test("Test Section: Choose From Multiple Different Style Layouts", async ({ page }) => {
+
+    const section_root = page.getByTestId('8caf97c')
+
+    await section_root.getByRole("heading", { name: "Choose From Multiple Different Style Layouts" }).scrollIntoViewIfNeeded();
+    await expect(section_root.getByRole("heading", { name: "Choose From Multiple Different Style Layouts" })).toBeVisible();
+    await expect(section_root.getByText("Beautifully present your content in an attractive ")).toBeVisible();
+    // Click Circle Item to Visibile Content 
+    await expect(section_root.locator('.fas.fa-leaf')).toBeVisible();
+    await expect(page.getByTestId('e1d6ade').locator('.eael-circle-btn-icon').getByText('Item 1')).toBeVisible();
+    await section_root.locator('.fas.fa-leaf').click();
+    await expect(section_root.locator('.eael-interactive-circle').getByText('Present your content in an attractive Circle layout item 1. You can highlight')).toBeVisible();
+
+
+  });
 });
