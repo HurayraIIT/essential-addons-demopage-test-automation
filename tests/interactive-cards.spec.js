985a46721f9e45bebdab13ba30de04c9c2019757
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-06-26 16:38:33 +0600

    Interactive_cards - Section 1 Test Case

--- tests/interactive-cards.spec.js
+++ tests/interactive-cards.spec.js
@@ -14,8 +14,22 @@ test.describe("Interactive Cards", () => {
     });
 
     test("Test Section: Limitless Flexible Design", async ({ page }) => {
+
+        const section_root = page.getByTestId('182b845')
+
         await page.getByRole("heading", { name: "Limitless Flexible Design" }).scrollIntoViewIfNeeded();
         await expect(page.getByRole("heading", { name: "Limitless Flexible Design" })).toBeVisible();
         await expect(page.getByText("Take your Elementor web design")).toBeVisible();
+
+        await expect(section_root.locator('.image-screen')).toBeVisible();
+        await section_root.locator('.image-screen').click();
+        await expect(section_root.locator('.image')).toBeVisible();
+        await expect(section_root.locator('#interactive-card-368e06c7').getByRole('heading', { name: 'Amazing Colorful City' })).toBeVisible();
+        await expect(section_root.locator('#interactive-card-368e06c7').getByText('We have only one earth to')).toBeVisible();
+        await expect(section_root.locator('#interactive-card-368e06c7').getByRole('link', { name: 'Read More' })).toBeVisible();
+        await expect(section_root.locator('.fas.fa-times')).toBeVisible();
+        await section_root.locator('.fas.fa-times').click();
+        await expect(section_root.locator('.image-screen')).toBeVisible();
     });
+
 });
