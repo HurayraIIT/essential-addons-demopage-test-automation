94147a83477fc5fe1f7396f5589a09e65e88b769
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-06-30 15:32:25 +0600

    One Page Navigation Test Case Done

--- tests/one-page-nav.spec.js
+++ tests/one-page-nav.spec.js
@@ -13,11 +13,9 @@ test.describe("One Page Navigation", () => {
         await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/creative-elements\/one-page-navigation/);
     });
 
-    test("Test Section: Create One Page Navigation", async ({ page }) => {
-        await page.getByRole("heading", { name: "Create One Page Navigation" }).scrollIntoViewIfNeeded();
-        await expect(page.getByRole("heading", { name: "Create One Page Navigation" })).toBeVisible();
-        await expect(page.getByText("Help your visitors navigate ")).toBeVisible();
-        // Nav Menu - Icon and Text Visibility Check 
+    // Nav Menu - Icon and Text Visibility Check
+
+    test("Test Section: Icon and Text Visibility", async ({ page }) => {
         await expect(page.getByRole('link', { name: '' })).toBeVisible();
         await page.getByRole('link', { name: '' }).hover();
         await expect(page.locator('#eael-one-page-nav-2221bcee').getByText('Home')).toBeVisible();
@@ -25,13 +23,20 @@ test.describe("One Page Navigation", () => {
         await page.locator('a').filter({ hasText: '.st0{fill:#1DA1F3;}' }).hover();
         await expect(page.getByText('Features')).toBeVisible();
         await expect(page.locator('a').filter({ hasText: '.st0{fill:#3AD0BF;}' })).toBeVisible();
-        await page.locator('a').filter({ hasText: '.st0{fill:#3AD0BF;}' }).hover();
-        await expect(page.getByText('Services')).toBeVisible();
         await expect(page.locator('a').filter({ hasText: '.st0{fill:#E067B3;}' })).toBeVisible();
-        await page.locator('a').filter({ hasText: '.st0{fill:#E067B3;}' }).hover();
-        await expect(page.getByText('Pricing Plans')).toBeVisible();
         await expect(page.locator('a').filter({ hasText: '.st0{fill:#8562F8;}' })).toBeVisible();
-        await page.locator('a').filter({ hasText: '.st0{fill:#8562F8;}' }).hover();
-        await expect(page.locator('#eael-one-page-nav-2221bcee').getByText('Blog')).toBeVisible();
+    });
+
+    // Click Nav Menu to Scroll Specific Section Check
+    test("Test Section: Scroll Specific Section", async ({ page }) => {
+        await page.locator('a[data-row-id="hero-area"]').click();
+        await page.waitForTimeout(700);
+        await expect(page.getByTestId('3b429615')).toBeInViewport();
+        await page.locator('a[data-row-id="features-section"]').click();
+        await page.waitForTimeout(700);
+        await expect(page.getByTestId('751ac69')).toBeInViewport();
+        await page.locator('a[data-row-id="blog-posts-section"]').click();
+        await page.waitForTimeout(700);
+        await expect(page.getByTestId('226fd8a7')).toBeInViewport();
     });
 });
