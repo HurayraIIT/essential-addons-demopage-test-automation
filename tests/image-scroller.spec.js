433d3aca1d14d026fa0bf1ba1d27db700a176c33
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-07-02 17:14:25 +0600

    Image Scroller

--- tests/image-scroller.spec.js
+++ tests/image-scroller.spec.js
@@ -17,5 +17,11 @@ test.describe("Image Scroller", () => {
         await page.getByRole("heading", { name: "Vertical Scrolling Effect On Hover" }).scrollIntoViewIfNeeded();
         await expect(page.getByRole("heading", { name: "Vertical Scrolling Effect On Hover" })).toBeVisible();
         await expect(page.getByText("Turn on the Auto Scroller, Choose ")).toBeVisible();
+
+
+        await page.getByRole('img', { name: 'Image Scroller 102' }).hover();
+        await expect(page.getByTestId('2e4d89f1').locator('//html/body/div[3]/div[1]/div/div/div/main/article/div/div/section[2]/div/div/div/section/div/div[2]/div/div/div/div/img')).toHaveCSS('transform', 'translateY(-133.75px)')
+
+
     });
 });
