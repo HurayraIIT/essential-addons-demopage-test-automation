844bf4a3fb71868c08cdba23782991269582406c
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-07-02 15:51:40 +0600

    Image Hotspots - Test Case

--- tests/image-hotspots.spec.js
+++ tests/image-hotspots.spec.js
@@ -5,7 +5,7 @@ let slug = "/image-hotspots";
 let heading = "Image Hotspot";
 
 test.describe("Filterable Gallery", () => {
-    // Setup
+    // Page Heading 
     test.beforeEach(async ({ page }) => {
         await page.goto(slug);
         await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
@@ -17,5 +17,17 @@ test.describe("Filterable Gallery", () => {
         await page.getByRole("heading", { name: "Make Image Interactive Using Hotspots" }).scrollIntoViewIfNeeded();
         await expect(page.getByRole("heading", { name: "Make Image Interactive Using Hotspots" })).toBeVisible();
         await expect(page.getByText("Put hotspots on the ")).toBeVisible();
+        // Image,icon and hover visibility test case 
+        await expect(page.getByRole('img', { name: 'Image Hotspots 102' })).toBeVisible();
+        await expect(page.locator('.eael-hot-spot-wrap').first()).toBeVisible();
+        await page.locator('.eael-hot-spot-wrap').first().hover();
+        await expect(page.getByText('Bag')).toBeVisible();
+        await expect(page.locator('a:nth-child(4)').first()).toBeVisible();
+        await page.locator('a:nth-child(4)').first().hover();
+        await expect(page.locator('div').filter({ hasText: 'Mug' }).nth(1)).toBeVisible();
+        await expect(page.locator('a:nth-child(5)').first()).toBeVisible();
+        await page.locator('a:nth-child(5)').first().hover();
+        await expect(page.locator('div').filter({ hasText: 'T-shirt' }).nth(1)).toBeVisible();
+
     });
 });
