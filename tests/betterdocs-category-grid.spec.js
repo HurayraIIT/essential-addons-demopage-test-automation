bad67dfe7d7451ad5d022f3cb6e2082bdaaef98c
Abu Hurayra <hurayraiit@gmail.com>
2024-08-14 12:48:03 +0600

    updated tests

--- tests/betterdocs-category-grid.spec.js
+++ tests/betterdocs-category-grid.spec.js
@@ -24,33 +24,3 @@ test.describe("BetterDocs Category Grid", () => {
     await expect(page.getByText("Make it available for any type of posts, besides")).toBeVisible();
   });
 });
-
-test.describe("BetterDocs Category Grid - Structure Tests", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(slug);
-  });
-
-  const target_selectors = [
-    { section_name: "EA BetterDocs Category Grid - Style 01", selector: ".elementor-element-36d08f5" },
-    { section_name: "EA BetterDocs Category Grid - Style 02", selector: ".elementor-element-f5f1aeb" },
-  ];
-
-  target_selectors.forEach((target) => {
-    test(target.section_name, async ({ page, browserName }) => {
-      if (browserName.toLowerCase() === "webkit") return;
-
-      const selector = target.selector;
-      await page.waitForSelector(selector);
-      await page.locator(selector).scrollIntoViewIfNeeded();
-      await page.waitForTimeout(400);
-
-      const filePath = path.join(__dirname, `../snapshots/${slug.substring(1)}-${selector.substring(1)}.json`);
-
-      const nodeStructure = await page.evaluate(evaluateNodeStructure, selector);
-      saveStructure(nodeStructure, filePath);
-
-      const existingNodeStructure = getStructure(filePath);
-      expect(nodeStructure).toEqual(existingNodeStructure);
-    });
-  });
-});
