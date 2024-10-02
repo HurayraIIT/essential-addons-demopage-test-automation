17da13c1dd2570f225088784db71316b8e60a568
obayedmamur <obayedmamur@gmail.com>
2024-10-02 23:39:36 +0600

    added test cases for CTA first section

--- tests/call-to-action.spec.js
+++ tests/call-to-action.spec.js
@@ -5,25 +5,33 @@ import { test, expect } from "../global-setup";
 let slug = "/call-to-action";
 let heading = "Call To Action";
 
-// test.describe("Call To Action", () => {
-//   // Page Heading
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto(slug);
-//     await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/call-to-action/);
-//   });
+test.describe("Call To Action", () => {
+  // Page Heading
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/call-to-action/);
 
-//   // Preset 1
-//   test("Test Section: Preset 1", async ({ page }) => {
-//     const section_root = page.getByTestId("514ccb5e");
+    // Scroll to the section
+    await page.getByText("Amaze Audience With Attractive CTA").scrollIntoViewIfNeeded();
+    await expect(page.getByText("Amaze Audience With Attractive CTA")).toBeVisible();
+    await expect(page.getByText("Style your Call To Action content, color and link up to direct visitors to desire site")).toBeVisible();
+  });
 
-//     await section_root.getByRole("heading", { name: "Essential Addons for Elementor" }).scrollIntoViewIfNeeded();
-//     await expect(section_root.getByRole("heading", { name: "Essential Addons for Elementor" })).toBeVisible();
-//     await expect(section_root.locator(".cta-preset-1")).toContainText(
-//       "Enhance your Elementor page building experience with 57+ creative "
-//     );
-//     await expect(section_root.getByRole("link", { name: "Purchase Now" })).toBeVisible();
-//     await expect(section_root.getByRole("img", { name: "Call To Action 102" })).toBeVisible();
-//   });
-// });
+  // First Section: Essential Addons for Elementor
+  test("Test Section: Essential Addons for Elementor", async ({ page }) => {
+    const section_root = page.getByTestId("c17a59b");
+
+    await section_root.getByRole("heading", { name: "Essential Addons for Elementor" }).scrollIntoViewIfNeeded();
+    await expect(section_root.getByRole("heading", { name: "Essential Addons for Elementor" })).toBeVisible();
+    await expect(section_root.locator(".cta-preset-1")).toContainText(
+      "Enhance your Elementor page building experience with 57+ creative "
+    );
+    await expect(section_root.getByRole("link", { name: "Purchase Now" })).toBeVisible();
+    await expect(section_root.locator('.cta-button  ')).toHaveCSS('background-color', 'rgb(129, 65, 255)');
+    await expect(section_root.locator('.cta-button  ')).toHaveCSS('font-family', 'Poppins, sans-serif');
+    await expect(section_root.locator('.cta-button  ')).toHaveCSS('font-size', '15px');
+    await expect(section_root.locator('.cta-button  ')).toHaveCSS('font-weight', '400');
+  });
+});
