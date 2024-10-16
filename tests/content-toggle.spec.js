3b9ba476ae7553e9fd15fda87eeda5414177a457
Abu Hurayra <hurayraiit@gmail.com>
2024-10-16 14:38:39 +0600

    Added a test for Content Toggle

--- tests/content-toggle.spec.js
+++ tests/content-toggle.spec.js
@@ -2,21 +2,41 @@
 
 import { test, expect } from "../global-setup";
 
-let slug = "https://essential-addons.com/elementor/content-toggle";
-let heading = "Content Toggle";
+let slug = "/content-elements/toggle/";
+let heading = "Content Toggle Hurayra Automation 241016";
 
-// test.describe("Content Toggle", () => {
-//   // Setup
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto(slug);
-//     await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/content-toggle/);
-//   });
+test.describe("Content Toggle https://qa1.site/go/pb4317", () => {
+  let toggle_loc = null;
 
-//   test("Test Section: Display Save Template For Content Toggle", async ({ page }) => {
-//     await page.getByRole("heading", { name: "Display Save Template For Content Toggle" }).scrollIntoViewIfNeeded();
-//     await expect(page.getByRole("heading", { name: "Display Save Template For Content Toggle" })).toBeVisible();
-//     await expect(page.getByText("Choose your save template designs & set")).toBeVisible();
-//   });
-// });
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await page.waitForLoadState("networkidle");
+    await page.getByRole("heading", { name: heading, exact: true }).scrollIntoViewIfNeeded();
+    await expect(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+
+    toggle_loc = page.locator(".elementor-element-6281f4d");
+  });
+
+  test("Test Content Tab & Toggle Should Work", async ({ page }) => {
+    // Label & Switch
+    await expect(page.getByText('Primary 241016')).toBeVisible();
+    await expect(toggle_loc.locator(".eael-toggle-switch-rectangle")).toBeVisible();
+    await expect(page.getByText('Secondary 241016')).toBeVisible();
+
+    // Only Primary Content should be visible
+    await expect(toggle_loc.locator(".eael-toggle-primary-wrap").locator("img")).toBeVisible();
+
+    await expect(page.getByText("Secondary Content 241016")).toBeHidden();
+    await expect(toggle_loc.locator(".eael-toggle-secondary-wrap").locator("img")).toBeHidden();
+
+    // Perform toggle
+    await toggle_loc.locator(".eael-toggle-switch-rectangle").click();
+    await page.waitForTimeout(500);
+
+    // Only Secondary Content should be visible
+    await expect(page.getByText('Secondary Content 241016')).toBeVisible();
+    await expect(toggle_loc.locator(".eael-toggle-secondary-wrap").locator("img")).toBeVisible();
+
+    await expect(toggle_loc.locator(".eael-toggle-primary-wrap").locator("img")).toBeHidden();
+  });
+});
