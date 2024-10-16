20b66a8bd0391acef11c242aa552ac49c300f8ea
Mohammad Obayed Mamur <obayedmamur@gmail.com>
2024-10-16 13:00:03 +0600

    Test Cases added for Basic Preset 1

--- tests/call-to-action.spec.js
+++ tests/call-to-action.spec.js
@@ -1,29 +1,44 @@
 "use strict";
 
+import exp from "constants";
 import { test, expect } from "../global-setup";
 
-let slug = "https://essential-addons.com/elementor/call-to-action";
+let slug = "marketing-elements/call-to-action";
 let heading = "Call To Action";
 
-// test.describe("Call To Action", () => {
-//   // Page Heading
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto(slug);
-//     await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute("href", /docs\/call-to-action/);
-//   });
+test.describe("Call To Action", () => {
+  let basicPreset1ID = "#basic-preset1";
+  let basicPreset1Locator = null;
+  // Page Heading
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
+    basicPreset1Locator = page.locator(basicPreset1ID);
+  });
 
-//   // Preset 1
-//   test("Test Section: Preset 1", async ({ page }) => {
-//     const section_root = page.getByTestId("514ccb5e");
-
-//     await section_root.getByRole("heading", { name: "Essential Addons for Elementor" }).scrollIntoViewIfNeeded();
-//     await expect(section_root.getByRole("heading", { name: "Essential Addons for Elementor" })).toBeVisible();
-//     await expect(section_root.locator(".cta-preset-1")).toContainText(
-//       "Enhance your Elementor page building experience with 57+ creative "
-//     );
-//     await expect(section_root.getByRole("link", { name: "Purchase Now" })).toBeVisible();
-//     await expect(section_root.getByRole("img", { name: "Call To Action 102" })).toBeVisible();
-//   });
-// });
+  // Basic Preset 1
+  test("Content Style: Basic | Content Preset: Preset 1", async ({ page }) => {
+    await expect.soft(page.getByText("Content Style: Basic | Content Preset: Preset 1")).toBeVisible();
+    await expect(basicPreset1Locator).toBeVisible();
+    await expect(basicPreset1Locator).toHaveClass(/elementor-widget-eael-cta-box/);
+    // Sub Title
+    await expect(basicPreset1Locator.locator(".sub-title")).toHaveCSS("font-size", "19px");
+    // Title
+    await expect(basicPreset1Locator.locator(".title")).toHaveCSS("font-size", "37px");
+    await expect(basicPreset1Locator.locator(".title")).toHaveCSS("font-weight", "700");
+    await expect(basicPreset1Locator.locator(".title")).toHaveCSS("color", "rgb(114, 28, 233)");
+    await expect(basicPreset1Locator.locator(".title")).toHaveCSS("font-family", "Poppins, sans-serif");
+    // Content
+    await expect(basicPreset1Locator.getByText("Enhance your Elementor page building experience with 100+ creative elements. Add powers to your page builder using our easy-to-use elements")).toBeVisible();
+    await expect(basicPreset1Locator.getByText("Enhance your Elementor page building experience with 100+ creative elements. Add powers to your page builder using our easy-to-use elements")).toHaveCSS("font-family", "\"Open Sans\", sans-serif");
+    await expect(basicPreset1Locator.getByText("Enhance your Elementor page building experience with 100+ creative elements. Add powers to your page builder using our easy-to-use elements")).toHaveCSS("color", "rgb(0, 0, 0)");
+    // Button
+    await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toBeVisible();
+    await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toBeEnabled();
+    // await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toHaveURL("https://eael.site");
+    await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toHaveClass("cta-button  effect-2");
+    await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toHaveCSS("box-shadow", "rgba(27, 33, 45, 0.72) 4px 4px 0px 0px");
+    await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toHaveCSS("font-size", "19px");
+    await expect(basicPreset1Locator.getByRole("link", {name: /Explore More/})).toHaveCSS("font-family", "Poppins, sans-serif");
+  });
+});
