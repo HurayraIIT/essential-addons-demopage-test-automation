bd53dfe38f974bf017948712c9da5d9b7e00009c
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-11-12 13:56:32 +0600

    Simple Menu Preset 1 Done

--- tests/simple-menu.spec.js
+++ tests/simple-menu.spec.js
@@ -1,45 +1,29 @@
 "use strict";
 import { test, expect } from "../global-setup";
-
-let slug = "/simple-menu";
+let slug = "/content-elements/simple-menu/";
 let heading = "Simple Menu";
-
-// test.describe("Simple Menu", () => {
-//   // Setup
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto(slug);
-//     await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toBeVisible();
-//     await expect(page.getByRole("link", { name: "Documentation" })).toHaveAttribute(
-//       "href",
-//       /docs\/content-elements\/simple-menu/
-//     );
-//   });
-
-//   test("Test Section: Style Default Skin & Horizontal Layout", async ({ page }) => {
-//     await page
-//       .getByTestId("3fcb4700")
-//       .getByRole("heading", { name: "Style Default Skin & Horizontal Layout" })
-//       .scrollIntoViewIfNeeded();
-//     await expect(
-//       page.getByTestId("3fcb4700").getByRole("heading", { name: "Style Default Skin & Horizontal Layout" })
-//     ).toBeVisible();
-//     await expect(
-//       page.getByTestId("48313d59").getByText("Add drop down menu icon, animation effect, hover effect")
-//     ).toBeVisible();
-//   });
-// });
-
-
 test.describe("Simple Menu", () => {
+    let presetID = null;
     test.beforeEach(async ({ page }) => {
         await page.goto(slug);
+        await page.waitForLoadState("networkidle");
+        await expect.soft(page.getByRole("heading", { name: heading, exact: true })).toBeVisible();
     });
-
-    test("Style Default Skin & Horizontal Layout Preset 1", async ({ page }) => {
-        const heading = page.getByRole('heading', { name: 'Style Default Skin & Horizontal Layout Preset 1' });
-        await heading.scrollIntoViewIfNeeded();
-        await expect(heading).toBeVisible();
+    test("Style Default Skin & Horizontal Layout | Preset 1", async ({ page }) => {
+        presetID = page.locator('#simpleMenu1');
+        // Ensure the basic preset menu is visible
+        await expect.soft(presetID).toBeVisible();
+        await expect.soft(presetID).toHaveClass(/elementor-widget-eael-simple-menu/);
+        // Validate menu items and their roles
+        const menuLocator = presetID.locator("#menu-header-menu");
+        await expect(menuLocator).toBeVisible(); // Validate the menu is visible
+        // Validate specific links in the menu
+        await expect.soft(menuLocator.getByRole("link", { name: "Home" })).toBeVisible();
+        await expect.soft(menuLocator.getByRole("link", { name: "Demos" })).toBeVisible();
+        //Validate speacific css applyed or not
+        await expect.soft(menuLocator.getByRole('link',{name:'Home'})).toHaveCSS('font-family','Risque, sans-serif');
+        await expect.soft(menuLocator.getByRole('link',{name:'Home'})).toHaveCSS('font-size','18px');
+        await expect.soft(menuLocator.getByRole('link',{name:'Home'})).toHaveCSS('font-weight','700');
+        await expect.soft(menuLocator.getByRole('link',{name:'Home'})).toHaveCSS('color','rgb(255, 255, 255)');
     });
-
 });
