cc32ae892059c656f5a6db759a54e1e61c961ba0
Abu Hurayra <hurayraiit@gmail.com>
2024-11-13 15:33:17 +0600

    added tests for woo product list

--- tests/woo-product-list.spec.js
+++ tests/woo-product-list.spec.js
@@ -76,9 +76,93 @@ test.describe("Woo Product List - Preset 1", () => {
     await expect.soft(imageWrapper).toHaveCSS("border-radius", "11px");
   });
 
-  // test("Test Style Tab > Content", async ({ page }) => {
-  //   //
-  // });
+  test("Test Style Tab > Content", async ({ page }) => {
+    //// Content Wrapper
+    // Padding 21px
+    let contentWrapper = widget.locator("div.eael-product-list-content-wrap").nth(1);
+    await expect.soft(contentWrapper).toHaveCSS("padding", "21px");
+
+    //// Content Header
+    // Margin 11px, padding 12px
+    let contentHeader = widget.locator("div.eael-product-list-content-header").nth(1);
+    await expect.soft(contentHeader).toHaveCSS("margin", "11px");
+    await expect.soft(contentHeader).toHaveCSS("padding", "12px");
+
+    //// Content Body
+    // Title: Margin 3px, Padding 4px
+    let title = widget.locator("h1.eael-product-list-title").nth(1);
+    await expect.soft(title).toHaveCSS("margin", "3px");
+    await expect.soft(title).toHaveCSS("padding", "4px");
+
+    // Excerpt: Margin 5px, Padding 6px
+    let excerpt = widget.locator("div.eael-product-list-excerpt").nth(1);
+    await expect.soft(excerpt).toHaveCSS("margin", "5px");
+    await expect.soft(excerpt).toHaveCSS("padding", "6px");
+
+    // price: Margin 7px, Padding 8px
+    let price = widget.locator("h3.eael-product-list-price").nth(1);
+    await expect.soft(price).toHaveCSS("margin", "7px");
+    await expect.soft(price).toHaveCSS("padding", "8px");
+
+    //// Content Footer
+    // Total Sold: Progress height 5px, Border Radius 12px
+    let progressBar = widget.locator("div.eael-product-list-progress-bar-outer").nth(1);
+    await expect.soft(progressBar).toHaveCSS("height", "5px");
+    await expect.soft(progressBar).toHaveCSS("border-radius", "12px");
+
+    // Static Buttons
+      // Add to cart: 
+      // Margin 5px
+      // Padding 21 21 21 41
+      // Border Radius 16px
+      // Border Type double
+      // Border width 3px
+      // Border color red
+      // Box Shadow 2 3 14 5
+    let footer = widget.locator("div.eael-product-list-content-footer").nth(1);
+    let addToCartButton = footer.locator("a.add_to_cart_button");
+    await expect.soft(addToCartButton).toHaveCSS("margin", "5px");
+    await expect.soft(addToCartButton).toHaveCSS("padding", "21px 21px 21px 41px");
+    await expect.soft(addToCartButton).toHaveCSS("border-radius", "16px");
+    await expect.soft(addToCartButton).toHaveCSS("border-style", "double");
+    await expect.soft(addToCartButton).toHaveCSS("border-width", "3px");
+    await expect.soft(addToCartButton).toHaveCSS("border-color", "rgb(255, 0, 0)");
+
+
+      // View Products: 
+      // Margin 6px
+      // Padding 22px
+      // Border Radius 17px
+      // Border Type dotted
+      // Border width 4px
+      // Border color green
+      // Box Shadow 1 2 13 4
+    let viewProductsButton = footer.getByText("View Product241113");
+    await expect.soft(viewProductsButton).toHaveCSS("margin", "6px");
+    await expect.soft(viewProductsButton).toHaveCSS("padding", "22px");
+    await expect.soft(viewProductsButton).toHaveCSS("border-radius", "17px");
+    await expect.soft(viewProductsButton).toHaveCSS("border-style", "dotted");
+    await expect.soft(viewProductsButton).toHaveCSS("border-width", "4px");
+    await expect.soft(viewProductsButton).toHaveCSS("border-color", "rgb(0, 255, 0)");
+    await expect.soft(viewProductsButton).toHaveCSS("box-shadow", "rgba(0, 0, 0, 0.5) 1px 2px 13px 4px");
+
+    // On Hover Buttons
+    // Margin 5px
+    // Padding 6px
+    // Border Radius 7px
+    // Border Type Dashed
+    // Border Width 7px
+    // Border color blue
+    // Box Shadow 3 4 15 6
+    let onHoverButtons = widget.locator("li.eael-product-list-add-to-cart-button a").nth(1);
+    await expect.soft(onHoverButtons).toHaveCSS("margin", "5px");
+    await expect.soft(onHoverButtons).toHaveCSS("padding", "6px");
+    await expect.soft(onHoverButtons).toHaveCSS("border-radius", "7px");
+    await expect.soft(onHoverButtons).toHaveCSS("border-style", "dashed");
+    await expect.soft(onHoverButtons).toHaveCSS("border-width", "7px");
+    await expect.soft(onHoverButtons).toHaveCSS("border-color", "rgb(0, 0, 255)");
+    await expect.soft(onHoverButtons).toHaveCSS("box-shadow", "rgba(0, 0, 0, 0.5) 3px 4px 15px 6px");
+  });
 
   test("Test Style Tab > Load More", async ({ page }) => {
     // Padding 15px
