d17b8507f5ee3edb9e70db59dc618449e06c2d79
Abu Hurayra <hurayraiit@gmail.com>
2024-12-15 23:46:46 +0600

    added tests for BetterDocs category box

--- tests/betterdocs-category-box.spec.js
+++ tests/betterdocs-category-box.spec.js
@@ -2,10 +2,78 @@
 
 import { test, expect } from "../global-setup";
 
-let slug = "https://essential-addons.com/elementor/betterdocs-category-box";
-let heading = "BetterDocs Category Box";
+let slug = "/documentation-elements/betterdocs-category-box/";
 
-test.describe("BetterDocs Category Box", () => {
+test.describe("BetterDocs Category Box - Layout One", () => {
+  let heading = "Hurayra Automation 241215 One";
+  let widget;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await page.waitForLoadState("networkidle");
+    let headingLocator = page.getByRole("heading", { name: heading, exact: true });
+    await headingLocator.scrollIntoViewIfNeeded();
+    await expect.soft(headingLocator).toBeVisible();
+    await headingLocator.click();
+
+    widget = page.getByTestId("bf36585");
+  });
+
+  test("Test Contents", async ({ page }) => {
+    await expect(widget).toMatchAriaSnapshot(`
+      - link "betterdocs-category-box-icon Setup Process pre-one1articles-one":
+        - img "betterdocs-category-box-icon"
+        - heading "Setup Process" [level=1]
+      - link "betterdocs-category-box-icon Installation pre-one2articles-one":
+        - img "betterdocs-category-box-icon"
+        - heading "Installation" [level=1]
+      - link "betterdocs-category-box-icon Getting Started pre-one5articles-one":
+        - img "betterdocs-category-box-icon"
+        - heading "Getting Started" [level=1]
+      - link "betterdocs-category-box-icon Configurations pre-one2articles-one":
+        - img "betterdocs-category-box-icon"
+        - heading "Configurations" [level=1]
+      `);
+  });
+});
+
+test.describe("BetterDocs Category Box - Layout Two", () => {
+  let heading = "Hurayra Automation 241215 Two";
+  let widget;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(slug);
+    await page.waitForLoadState("networkidle");
+    let headingLocator = page.getByRole("heading", { name: heading, exact: true });
+    await headingLocator.scrollIntoViewIfNeeded();
+    await expect.soft(headingLocator).toBeVisible();
+    await headingLocator.click();
+
+    widget = page.getByTestId("e2168ee");
+  });
+
+  test("Test Contents", async ({ page }) => {
+    await expect(widget).toMatchAriaSnapshot(`
+    - link "betterdocs-category-box-icon Configurations 2":
+      - img "betterdocs-category-box-icon"
+      - heading "Configurations" [level=2]
+    - link "betterdocs-category-box-icon Getting Started 5":
+      - img "betterdocs-category-box-icon"
+      - heading "Getting Started" [level=2]
+    - link "betterdocs-category-box-icon Installation 2":
+      - img "betterdocs-category-box-icon"
+      - heading "Installation" [level=2]
+    - link "betterdocs-category-box-icon Setup Process 1":
+      - img "betterdocs-category-box-icon"
+      - heading "Setup Process" [level=2]
+    `);
+  });
+});
+
+test.describe("BetterDocs Category Box - Live Demo Page", () => {
+  let slug = "https://essential-addons.com/elementor/betterdocs-category-box";
+  let heading = "BetterDocs Category Box";
+
   let layout1DataID = "5655788f";
   let layout2DataID = "783034e0";
   test.beforeEach(async ({ page }) => {
