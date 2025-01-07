59eedb6876e3e8df1c77465c9f1f421651024e65
Mohammad Obayed Mamur <obayedmamur@gmail.com>
2025-01-07 14:04:37 +0600

    Add Test Cases for All The Controls and their Contents

--- tests/filterable-gallery.spec.js
+++ tests/filterable-gallery.spec.js
@@ -22,16 +22,44 @@ test.describe("Filterable Gallery", () => {
   let masonryCardLocator = null;
   let masonrySearchFilterLocator = null;
 
+  let galleryItem1 = null;
+  let galleryItem2 = null;
+  let galleryItem3 = null;
+  let galleryItem4 = null;
+  let galleryItem5 = null;
+  let galleryItem6 = null;
+  let galleryItem7 = null;
+  let galleryItem8 = null;
+  let galleryItem9 = null;
+  let galleryItem10 = null;
+  let galleryItem11 = null;
+  let galleryItem12 = null;
 
   // Page Heading
   test.beforeEach(async ({ page }) => {
     await page.goto(slug);
+    await page.waitForLoadState("networkidle");
     gridOverlayLocator = page.locator(gridOverlayID);
     gridCardLocator = page.locator(gridCardID);
     gridSearchFilterLocator = page.locator(gridSearchFilterID);
     masonryOverlayLocator = page.locator(masonryOverlayID);
     masonryCardLocator = page.locator(masonryCardID);
     masonrySearchFilterLocator = page.locator(masonrySearchFilterID);
+
+    galleryItem1 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-a1b2c3").nth(0);
+    galleryItem2 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-a1b2c3").nth(1);
+    galleryItem3 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-a1b2c3").nth(2);
+    galleryItem4 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-d4e5f6").nth(0);
+    galleryItem5 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-d4e5f6").nth(1);
+    galleryItem6 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-d4e5f6").nth(2);
+    galleryItem7 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-g7h8i9").nth(0);
+    galleryItem8 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-g7h8i9").nth(1);
+    galleryItem9 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-g7h8i9").nth(2);
+    galleryItem10 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-j10k11l12").nth(0);
+    galleryItem11 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-j10k11l12").nth(1);
+    galleryItem12 = page.locator(".eael-filterable-gallery-item-wrap.eael-cf-j10k11l12").nth(2);
+
+
   });
 
   // Grid Style & Layout Combinations Start
@@ -60,23 +88,115 @@ test.describe("Filterable Gallery", () => {
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(2)).not.toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filterable-gallery-item-wrap eael-cf-d4e5f6")).not.toBeVisible();
 
+    await galleryItem1.hover(); //Grid Overlay Item 1
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 1'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 1.')).toBeVisible();
+    await expect(galleryItem1.locator('img')).toHaveAttribute('src', /image-01\.png/);
+
+    await galleryItem2.hover(); //Grid Overlay Item 2
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 2'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 2.')).toBeVisible();
+    await expect(galleryItem2.locator('img')).toHaveAttribute('src', /image-02\.png/);
+
+    await galleryItem3.hover(); //Grid Overlay Item 3
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 3'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 3.')).toBeVisible();
+    await expect(galleryItem3.locator('img')).toHaveAttribute('src', /image-03\.png/);
+
     await gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(2).click();
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(2)).toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(1)).not.toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filterable-gallery-item-wrap eael-cf-g7h8i9")).not.toBeVisible();
 
+    await galleryItem4.hover(); //Grid Overlay Item 4
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 4'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 4.')).toBeVisible();
+    await expect(galleryItem4.locator('img')).toHaveAttribute('src', /image-04\.png/);
+
+    await galleryItem5.hover(); //Grid Overlay Item 5
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 5'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 5.')).toBeVisible();
+    await expect(galleryItem5.locator('img')).toHaveAttribute('src', /image-05\.png/);
+
+    await galleryItem6.hover(); //Grid Overlay Item 6
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 6'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 6.')).toBeVisible();
+    await expect(galleryItem6.locator('img')).toHaveAttribute('src', /image-06\.png/);
 
     await gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(3).click();
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(3)).toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(2)).not.toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filterable-gallery-item-wrap eael-cf-j10k11l12")).not.toBeVisible();
 
+    await galleryItem7.hover(); //Grid Overlay Item 7
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 7'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 7.')).toBeVisible();
+    await expect(galleryItem7.locator('img')).toHaveAttribute('src', /7\.png/);
+
+    await galleryItem8.hover(); //Grid Overlay Item 8
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 8'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 8.')).toBeVisible();
+    await expect(galleryItem8.locator('img')).toHaveAttribute('src', /8\.png/);
+
+    await galleryItem9.hover(); //Grid Overlay Item 9
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 9'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 9.')).toBeVisible();
+    await expect(galleryItem9.locator('img')).toHaveAttribute('src', /9\.png/);
+
     await gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(4).click();
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(4)).toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(3)).not.toHaveClass("control active");
     await expect.soft(gridOverlayLocator.locator(".eael-filterable-gallery-item-wrap eael-cf-a1b2c3")).not.toBeVisible();
 
+    await galleryItem10.hover(); //Grid Overlay Item 10
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 10'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 10.')).toBeVisible();
+    await expect(galleryItem10.locator('img')).toHaveAttribute('src', /10\.png/);
+
+    await galleryItem11.hover(); //Grid Overlay Item 11
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 11'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 11.')).toBeVisible();
+    await expect(galleryItem11.locator('img')).toHaveAttribute('src', /11\.png/);
+
+    await galleryItem12.hover(); //Grid Overlay Item 12
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 12'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 12.')).toBeVisible();
+    await expect(galleryItem12.locator('img')).toHaveAttribute('src', /12\.png/);
+
+    // Perform Filter using Control (All) and Check All Gallery Items are Visible
     await gridOverlayLocator.locator(".eael-filter-gallery-control").locator('li').nth(0).click();
+    await galleryItem1.hover(); //Grid Overlay Item 1
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 1'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 1.')).toBeVisible();
+    await expect(galleryItem1.locator('img')).toHaveAttribute('src', /image-01\.png/);
+    await galleryItem5.hover(); //Grid Overlay Item 5
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 5'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 5.')).toBeVisible();
+    await expect(galleryItem5.locator('img')).toHaveAttribute('src', /image-05\.png/);
+    await galleryItem9.hover(); //Grid Overlay Item 9
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 9'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 9.')).toBeVisible();
+    await expect(galleryItem9.locator('img')).toHaveAttribute('src', /9\.png/);
+    await galleryItem12.hover(); //Grid Overlay Item 12
+    await page.waitForTimeout(500);
+    await expect.soft(gridOverlayLocator.getByRole('heading', {name: 'Grid Overlay Item 12'})).toBeVisible();
+    await expect.soft(gridOverlayLocator.getByText('Content of Grid Overlay Item 12.')).toBeVisible();
+    await expect(galleryItem12.locator('img')).toHaveAttribute('src', /12\.png/);
 
   });
 
